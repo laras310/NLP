c0795e65c4801542be2376c3cf07f4592d0f2f16
Ihza-Fajrur <84057899+Ihza-Fajrur@users.noreply.github.com>
2022-12-01 12:59:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, request
 
 app = Flask(__name__)
 app.secret_key = 'secret'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return 204
+    if request.method == 'POST':
+        document_1 = request.form.get('document_1', None)
+        document_2 = request.form.get('document_2', None)
+        
+        # if document_1 is not None
+        if document_1:
+            # if document_2 is None then processing document_1 without document_2
+            if not document_2:
+                # passing document_1 to the nlp function
+                result = None
+                return render_template('home.html', result=result), 200
+            
+            # passing document_1 and document_2 to the nlp function
+            result = None
+            return render_template('home.html', result=result), 200
+        
+        return 404
+    
+    elif request.method == 'GET':
+        return render_template('page.html'), 200
+    
+    return 404
+
+# @app.route('/about')
+# def about():
+    
+# @app.route('/contact')
+# def contact():
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
